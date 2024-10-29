f03c5c4c46328b3a239489dfee8e231e06daeaf5
Joaoof <joaodeus400@gmail.com>
2024-10-29 10:35:39 -0300

    feat: add products service

---
+++ ES.Services.API/Aggregates/ProdutcsAggregates/Interfaces/IProductsServices.cs
@@ -0,0 +1,9 @@
+﻿using ES.Domain.API.Models;
+
+namespace ES.Services.API.Aggregates.ProdutcsAggregates.Interfaces
+{
+    public interface IProductsServices
+    {
+        Task<ProductsModel> GetInformationProducts(int id); 
+    }
+}
---
+++ ES.Services.API/Aggregates/ProdutcsAggregates/Services/ProdutcsAppService.cs
@@ -0,0 +1,14 @@
+﻿]using ES.Domain.API.Models;
+]using ES.Services.API.Aggregates.ProdutcsAggregates.Interfaces;
+
+namespace ES.Services.API.Aggregates.ProdutcsAggregates.Services
+{
+    public class ProdutcsAppService : IProductsServices
+    {
+        private readonly IProductsRepository _productsRepository;
+        public Task<ProductsModel> GetInformationProducts(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
