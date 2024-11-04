57640b095a368c28816d80dabe621e064759fef7
Joao de Deus <joaodeus400@gmail.com>
2024-11-04 15:15:40 -0300

    feat: add viewModel products

---
+++ ES.Services.API/Aggregates/ProdutcsAggregates/ProductsViewModels/Request/ProductsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ES.Services.API.Aggregates.ProdutcsAggregates.ProductsViewModels.Request
+{
+    public class ProductsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string SKUCode { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+        public bool IsActive { get; set; } 
+
+    }
+}
