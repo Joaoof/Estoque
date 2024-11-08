5a7aa6f2c1e0acabe2719bf6d730078926714475
Joaoof <joaodeus400@gmail.com>
2024-11-08 08:33:43 -0300

    feat: add config cookies

--- ES.Application.API/Program.cs
+++ ES.Application.API/Program.cs
@@ -1,5 +1,6 @@
 using ES.Application.API.Configurations;
 using ES.Infra.API.Context;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 
 //using ES.Services.API.FilterMessage;
@@ -34,6 +35,21 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<EstoqueContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("ES.Infra.API")).EnableSensitiveDataLogging());
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<EstoqueContext>().AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 10;
+    options.Password.RequiredUniqueChars = 3;
+    options.Password.RequireNonAlphanumeric = false;
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "AspNetCore.Cookies";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+        options.SlidingExpiration = true;
+    });
 
 builder.Services.AddDatabase(_configuration);
 
